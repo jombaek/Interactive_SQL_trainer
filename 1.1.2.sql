578d241821469add47f64e39ee22eb536fa6fe89
Evgeniia <zhenyamestnik@gmail.com>
2023-03-27 00:01:50 +0700

    1.1 Relation (table)

---
+++ 1.1.2.sql
@@ -0,0 +1,5 @@
+-- https://stepik.org/lesson/297508/step/9?unit=279268
+-- Вставка записи в таблицу
+
+INSERT INTO book (title, author, price, amount)
+VALUES ('Мастер и Маргарита', 'Булгаков М.А.', 670.99, 3);
---
+++ 1.1.3.sql
@@ -0,0 +1,8 @@
+-- https://stepik.org/lesson/297508/step/10?unit=279268
+
+INSERT INTO book (title, author, price, amount)
+VALUES ('Белая гвардия', 'Булгаков М.А.', 540.50, 5);
+INSERT INTO book (title, author, price, amount)
+VALUES ('Идиот', 'Достоевский Ф.М.', 460, 10);
+INSERT INTO book (title, author, price, amount)
+VALUES ('Братья Карамазовы', 'Достоевский Ф.М.', 799.01, 2);
