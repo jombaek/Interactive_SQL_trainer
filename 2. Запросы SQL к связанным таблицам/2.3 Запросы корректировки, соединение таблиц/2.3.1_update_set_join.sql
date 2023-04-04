fa61b1e8252790f109575e78ebdec7dc76b16d53
Evgeniia <zhenyamestnik@gmail.com>
2023-04-04 23:23:39 +0700

    2.3 Adjustment queries, joining tables

---
+++ 2. Запросы SQL к связанным таблицам/2.3 Запросы корректировки, соединение таблиц/2.3.1_update_set_join.sql
@@ -0,0 +1,13 @@
+-- https://stepik.org/lesson/308887/step/2?unit=291013
+-- Запросы на обновление, связанные таблицы
+
+UPDATE book
+     INNER JOIN author ON author.author_id = book.author_id
+     INNER JOIN supply ON book.title = supply.title AND author.name_author = supply.author
+SET book.amount = book.amount + supply.amount,
+    book.price = (book.price * book.amount + supply.price * supply.amount) / (book.amount + supply.amount),
+    supply.amount = 0
+WHERE book.price <> supply.price;
+
+SELECT * FROM book;
+SELECT * FROM supply;
