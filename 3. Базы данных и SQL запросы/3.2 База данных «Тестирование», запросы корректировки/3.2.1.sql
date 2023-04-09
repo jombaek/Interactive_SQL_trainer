3aa63b14ff972fbe5d99f9fa15bd2e3e1e594aed
Evgeniia <zhenyamestnik@gmail.com>
2023-04-09 23:19:38 +0700

    3.2 Database "Testing", correction queries

---
+++ 3. Базы данных и SQL запросы/3.2 База данных «Тестирование», запросы корректировки/3.2.1.sql
@@ -0,0 +1,16 @@
+-- https://stepik.org/lesson/310422/step/2?unit=292728
+-- Задание
+
+INSERT INTO attempt (student_id, subject_id, date_attempt)
+SELECT student_id, subject_id, NOW()
+FROM
+	attempt
+	INNER JOIN student USING (student_id)
+	INNER JOIN subject USING (subject_id)
+WHERE name_student = 'Баранов Павел' AND name_subject = 'Основы баз данных';
+
+INSERT INTO attempt (student_id, subject_id, date_attempt)
+SELECT
+	SELECT student_id FROM student WHERE name_student = 'Баранов Павел',
+	SELECT subject_id FROM subject WHERE name_subject = 'Основы баз данных',
+	NOW();
